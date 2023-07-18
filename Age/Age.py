e811036c5e972abc032aaa6c99a405e96cd537b5
YitingLi0308 <yli54@nd.edu>
2023-07-18 14:09:15 -0400

    Updating Age Cache Algorithm

--- Age/Age.py
+++ Age/Age.py
@@ -8,39 +8,39 @@ file_list = []
 input_file = "test_data_updated.csv"
 with open(input_file, "r") as file:
     reader = csv.DictReader(file)
-    for row in reader:
-        access_times = row['access_times'].strip('[]').replace("'", "").split(", ")
-        access_times = [datetime.strptime(time, "%Y-%m-%d %H:%M:%S") for time in access_times if time]
-        row['access_times'] = access_times
-        file_list.append(row)
-
-file_list.sort(key=lambda x: x['access_times'][-1] if x['access_times'] else datetime.min)
+    rows = list(reader)
 
 # Threshold changed here
 maximum_time= int(os.environ.get("MAXIMUM_SIZE", 50))
-cutoff_date = datetime.now() - timedelta(days=maximum_time)
+cutoff =  timedelta(days=maximum_time)
 
 delete_files = []
 kept_files = []
 output_data = []  # List to store the output data
+file_state = {}
+last_access = {}
 
-for file_data in file_list:
-    last_accessed_date = file_data['access_times'][-1] if file_data['access_times'] else datetime.min
+# 0:
+# 1:
+# 2:
+for row in rows:
+    file_id = row["file_id"]
+    access_time = row["access_times"]
+    access_time = datetime.strptime(access_time, '%Y-%m-%d %H:%M:%S')
 
-    if last_accessed_date < cutoff_date:
-        delete_files.append(file_data)
-        output_data.append({
-            'access_time': last_accessed_date,
-            'keep or delete': 'D',
-            'file_id': file_data['file_id']
-        })
+    file_new_state = file_state.get(file_id,0)
+    if file_new_state == 2:
+        output_data.append({"access_time": access_time, "state": "M", "file_id": file_id})
+        continue
+    previous_access_date=last_access.get(file_id,access_time)
+    last_access[file_id] = access_time
+
+    if access_time - previous_access_date < cutoff:
+        file_state[file_id] = 1
+        output_data.append({"access_time": access_time, "state": "H", "file_id": file_id})
     else:
-        kept_files.append(file_data)
-        output_data.append({
-            'access_time': last_accessed_date,
-            'keep or delete': 'K',
-            'file_id': file_data['file_id']
-        })
+        file_state[file_id] = 2
+        output_data.append({"access_time": access_time, "state": "M", "file_id": file_id})
 
 # Write the output data to a CSV file
 file = sys.stdout
@@ -48,7 +48,7 @@ file.write(f"# Created: {datetime.now().isoformat()}\n")
 file.write(f"# Policy: Age\n")
 file.write(f"# Input: {input_file}\n")
 file.write(f"# Maximum time: {maximum_time / 365} year(s)\n")
-headerNames = ["access_time", "keep or delete", "file_id"]
+headerNames = ["access_time", "state", "file_id"]
 writer = csv.DictWriter(file, fieldnames=headerNames)
 writer.writeheader()
 writer.writerows(output_data)
--- Age/Sweep.sh
+++ Age/Sweep.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for i in 1 2 3 6 9 10 15 20; do
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
   MAXIMUM_SIZE=$((365 * $i))
   export MAXIMUM_SIZE
+  echo $MAXIMUM_SIZE
   python3 Age.py > output_file_$i.csv
 done
